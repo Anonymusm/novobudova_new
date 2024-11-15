593a4d0657d21766fb20d6f243b7d33e81a43057
Anonymusm <141570541+Anonymusm@users.noreply.github.com>
2024-11-15 05:38:26 +0000

    Deploying to gh-pages from @ Anonymusm/novobudova_new@638903e211f2a25c3c81041986dbcbd4f4e896fb 🚀

--- index.e970f442.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".header__icon").addEventListener("click",(function(){this.classlist.toggle("active"),document.querySelector(".navigation-list").classList.toggle("open")}));
-//# sourceMappingURL=index.e970f442.js.map
