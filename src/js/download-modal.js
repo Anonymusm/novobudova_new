c41ecb00e284361b8986a039c36855437288a733
Anton <141570541+Anonymusm@users.noreply.github.com>
2024-11-18 17:00:50 +0100

    Чекаю Галері Меланії

--- src/js/download-modal.js
+++ src/js/download-modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-backdrop]"),
+    openModalBtn: document.querySelector("[apart-modal-open]"),
+    closeModalBtn: document.querySelector("[apart-modal-close]"),
+    modal: document.querySelector("[apart-backdrop]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
